7a884707e60ba8063762f109fc74fe7628d55dee
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-18 13:49:16 +0530

    last commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT;
+
+const connect = require("./src/config/db");
+
+const appDataController = require("./src/controllers/appData.controlers");
+const watchListController = require("./src/controllers/watchLiist.controllers");
+
+app.use(cors());
+app.use(express.json());
+
+app.use(appDataController);
+app.use(watchListController);
+
+app.listen(port, async () => {
+  await connect();
+  console.log("Listening on post " + port);
+});
+
+module.exports = app;
